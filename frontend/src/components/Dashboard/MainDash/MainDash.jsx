189407c8845ae6bc1ac673520c4b631b72caed44
chathurkavishmantha <chathurkavish@gmail.com>
2023-01-22 09:11:11 +0530

    Added new folder and add frontend folder changes.it has main dash inside the navbar

--- frontend/src/components/Dashboard/MainDash/MainDash.jsx
+++ frontend/src/components/Dashboard/MainDash/MainDash.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 import '../../Dashboard/MainDash/MainDash.css'
 import Cards from '../Cards/Cards'
 import Table from '../Table/Table'
-
+import TopNav from '../../Layout/TopNav/TopNav';
 const MainDash = () => {
   return (
     <div className='MainDash'>
+      <div className="nav-menu">
+      <TopNav />
+      </div>
         <h1>Dashboard</h1>
         <Cards/>
         <h2>Recent Orders</h2>
--- frontend/src/components/Layout/Layout.jsx
+++ frontend/src/components/Layout/Layout.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import Router from '../../Router/Router';
 import Sidebar from './Sidebar/Sidebar';
-import TopNav from './TopNav/TopNav';
+
 
 
 const Layout = () => {
   return (
     <div className="App">
+      
       <div className='AppGlass'>
         <Sidebar/>
           <div className="App-Top-Nav">
-            <TopNav />
-                <Router />
+            
+            <Router />
           </div>
+          
       </div>
     </div>
   )
